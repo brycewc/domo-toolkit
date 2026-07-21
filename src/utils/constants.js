f9a1fb980ca15fe3d641bf52fbac9ef2178a584d
brycewc <bryce.cindrich@gmail.com>
2026-07-21 17:19:58 -0600

    Added approval constant page

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -59,6 +59,7 @@ export const ACTION_COLOR_PATTERNS = {
 export const SECTION_TITLES = {
   '/admin/people': 'People',
   '/app-studio': 'App Studio Apps',
+  '/approval': 'Approvals',
   '/codeengine': 'Code Engine Packages',
   '/datacenter/accounts': 'Accounts',
   '/datacenter/beastmode': 'Beast Mode',
